3f55264cc0c5bf83dfac81f977f34aeaf973220d
davidpjetraj <118452149+davidpjetraj@users.noreply.github.com>
2023-02-02 17:54:07 +0100

    Ndryshim ne PHP

---
+++ register/admin_users.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE 'db';
+
+CREATE TABLE `admin_users` (
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `create_datetime` date NOT NULL);
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `comment` text NOT NULL,
+  `create_datetime` date NOT NULL);
