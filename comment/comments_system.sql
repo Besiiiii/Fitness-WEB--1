209b599bf7117c307bb395cad5a074ab92510d95
davidpjetraj <davidpjetraj03@gmail.com>
2023-02-09 22:50:15 +0100

    ndryshime ne  sql comment

--- comment/comments_system.sql
+++ comment/comments_system.sql
@@ -1,29 +1,11 @@
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
---
--- Database: `comments_system`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_comment`
---
-
 CREATE TABLE `tbl_comment` (
-  `comment_id` int(11) NOT NULL,
+  `comment_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `parent_comment_id` int(11) DEFAULT NULL,
   `comment` text NOT NULL,
   `comment_sender_name` varchar(200) NOT NULL,
   `comment_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `tbl_comment`
---
 
 INSERT INTO `tbl_comment` (`comment_id`, `parent_comment_id`, `comment`, `comment_sender_name`, `comment_at`) VALUES
 (1, 0, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
@@ -32,23 +14,3 @@ nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
  'Vincy', '2022-06-30 09:57:11'),
 (2, 1, 'Okay, thank you for your comment.', 'Admin', '2022-06-30 09:57:31');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `tbl_comment`
---
-ALTER TABLE `tbl_comment`
-  ADD PRIMARY KEY (`comment_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `tbl_comment`
---
-ALTER TABLE `tbl_comment`
-  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-COMMIT;
