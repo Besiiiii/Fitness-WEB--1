3fbc548280305f826e2aab6de8cc3b8bd615cdee
davidpjetraj <davidpjetraj03@gmail.com>
2023-02-10 01:00:25 +0100

    dashbaord

--- register/dashboard.php
+++ register/dashboard.php
@@ -43,7 +43,7 @@ if (!isset($_SESSION['ID'])) {
                         <?php if ($_SESSION['ROLE'] == 'admin') { ?>
                             <li >
                                 <a href="dashboard.php" class="active">
-                                    <span data-feather="home"></span>
+                                    <span data-feather="users"></span>
                                     Dashboard <span></span>
                                 </a>
                             </li>
